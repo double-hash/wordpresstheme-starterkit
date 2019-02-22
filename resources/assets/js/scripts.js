ee3c0e796c8d374ed8354455f5ae1c46818597e4
Hugo <hugo@variable.club>
2019-02-22 10:55:39 +0100

    resources file update

--- resources/assets/js/scripts.js
+++ resources/assets/js/scripts.js
@@ -1,6 +1,20 @@
-var $ = require("jquery");
+(function($) {
 
-$( document ).ready(function() {
-	//...
-	
-});
+  // var jQueryBridget = require('jquery-bridget');
+
+  $( document ).ready(function() {
+  	// scripts
+
+    var lazysizes = require('lazysizes');
+    // var imagesLoaded = require('imagesloaded');
+    // var masonry = require('masonry-layout');
+
+    // make the jQuery plugins
+    // imagesLoaded.makeJQueryPlugin( $ );
+    // jQueryBridget( 'masonry', masonry, $ );
+
+    // modules
+    // require('./modules/_gallery.js');
+  });
+
+})(jQuery);
