e81400d699a37c1ddddbba4f9ab532bbe3e9609e
Hugo <hugo@variable.club>
2019-02-22 10:56:52 +0100

    adding template-parts folder

--- index.php
+++ index.php
@@ -12,4 +12,8 @@
 
 get_header(); ?>
 
+<main class="wrapper">
+  <?php get_template_part('template-parts/content'); ?>
+</main>
+
 <?php get_footer();
---
+++ template-parts/content.php
@@ -0,0 +1,1 @@
+<h1>Wordpress Theme Development StarterKit</h1>
